33b93a51fecb19a75ad41645aa158b26f0dc0f02
merciaw <mercia.wijaya@gmail.com>
2017-11-28 12:51:21 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,13 @@
+power <- read.table("household_power_consumption.txt",sep=";", na.strings="?",header=TRUE)
+power$Date <- as.Date(power$Date,"%d/%m/%Y")
+power <- power[power$Date=="2007-02-01"|power$Date=="2007-02-02",]
+
+power$Day <- wday(power$Date)
+power$Day[power$Day==5] <- "Thu"
+power$Day[power$Day==6] <- "Fri"
+power$Day[power$Day==7] <- "Sat"
+power$DateTime <- with(power, as.POSIXct(paste(as.Date(Date, format="%Y-%m-%d"), Time)))
+
+png(file="plot2.png", width=480, height=480)
+plot(power$DateTime,power$Global_active_power, type="l", ylab="Global Active Power (kilowatts)",xlab="")
+dev.off()
